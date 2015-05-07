d7950ec3524200b470d22eff0071a8628d0982c3
Lay Le <lay.le@oracle.com>
2015-05-07 11:03:02 -0700

    Use artifactory

--- ServicePublisher/src/main/java/com/oracle/ofm/cloud/qa/publisher/HelloWorldService.java
+++ ServicePublisher/src/main/java/com/oracle/ofm/cloud/qa/publisher/HelloWorldService.java
@@ -7,6 +7,7 @@ public class HelloWorldService {
     public String sayHello (String who) {
         return "Hello World, " + who;
         //Comment out the line above and uncomment the line below for the fix in ServiceConsumer junit test
+        //also add "import org.apache.commons.lang3.StringUtils;"
 //        return "Hello World, " + StringUtils.trim(who);
     }
 }
