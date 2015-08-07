b9171cc16d98f41ad7499f18bdbb0e6f357acdca
Lay Le <lay.le@oracle.com>
2015-08-07 14:33:01 -0700

    Removing Artifactory sturff

--- ServicePublisher/src/main/java/org/layle/dep/publisher/HelloWorldService.java
+++ ServicePublisher/src/main/java/org/layle/dep/publisher/HelloWorldService.java
@@ -1,13 +1,15 @@
 package org.layle.dep.publisher;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * Created by layle on 5/1/15.
  */
 public class HelloWorldService {
     public String sayHello (String who) {
-        return "Hello World, " + who;
+//        return "Hello World, " + who;
         //Comment out the line above and uncomment the line below for the fix in ServiceConsumer junit test
         //also add "import org.apache.commons.lang3.StringUtils;"
-//        return "Hello World, " + StringUtils.trim(who);
+        return "Hello World, " + StringUtils.trim(who);
     }
 }
